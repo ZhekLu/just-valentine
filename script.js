53b59a130bd66cf3bcc2b17c97968c3b9194a654
ZhekLu <justletmego@list.ru>
2024-09-08 16:04:00 +0300

    sorry page

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const alternatives = [
+const alternatives_love = [
     {text:"", images:"images/mashrooms.jpg"},
     {text:"Мы будем лениться вместе на выходных.", images:"images/chill.jpg"},
     {text:"Спиздим вместе половину твоего офиса?", images:"images/steal.jpg"},
     {text:"А потом будем победно беситься? Хи-хи-хи, попробуй отказать:)", images:"images/dance.jpg"},
     {text:"Подумай еще раз. С кем еще ты будешь засирать всех окружающих?", images:"images/anger.jpeg"},
 ];
-const ohyes = {text:"Знал, что ты выберешь меня, умничка.", images:"images/final.jpg"};
+
+const alternatives = [
+    {text:"", images:"gifs_sorry/1.gif"},
+    {text:"Я честно честно не хотел тебя обидеть:(", images:"gifs_sorry/2.gif"},
+    {text:"Я больше не буду тебя обижать...", images:"gifs_sorry/3.gif"},
+    {text:"А еще я сильно сильно тебя люблю", images:"gifs_sorry/4.gif"},
+    {text:"Давай тратить время на любовь, а не на ссоры", images:"gifs_sorry/5.gif"},
+];
+
+const ohyes_love = {text:"Знал, что ты выберешь меня, умничка.", images:"images/final.jpg"};
+const ohyes = {text:"Ура ура ура. Ты моя самая любимая любимость!", images:"gifs_sorry/final.gif"};
+
 const title = document.querySelector('.title');
 const text = document.querySelector('.text');
 const cat = document.querySelector('.cat');
